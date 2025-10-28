2f55003a5e83978b02a36ec01f1c50dc5af0dc4b
kenneth <kennethbacaltos091@gmail.com>
2025-10-28 15:38:01 +0800

    Fixed tab active state. Next TODO: Add finish setting profile component.

--- src/components/navigationBar/navigationItem.tsx
+++ src/components/navigationBar/navigationItem.tsx
@@ -1,21 +1,34 @@
 import type { LucideProps } from "lucide-react";
 import { Link } from "react-router";
 
+type Ttab = "Home" | "Chats" | "Manage" | "Menu";
+
 type NavigationItemProps = {
   Icon: React.ForwardRefExoticComponent<
     Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
   >;
   label: string;
   url: string;
+  onClick: (param: Ttab) => void;
+  activeTab: string;
 };
 
 export default function NavigationItem({
   Icon,
   label,
   url,
+  onClick,
+  activeTab,
 }: NavigationItemProps) {
   return (
-    <div className="hover:cursor-pointer hover:text-green-700">
+    <div
+      className={
+        activeTab === label
+          ? "hover:cursor-pointer text-green-700"
+          : "hover:cursor-pointer hover:text-green-700"
+      }
+      onClick={() => onClick(label as Ttab)}
+    >
       <Link className="flex flex-col items-center" to={url}>
         <Icon size={25} />
         <h1 className="font-semibold text-sm capitalize">{label}</h1>
--- src/pages/landlord/landlordDashboard.tsx
+++ src/pages/landlord/landlordDashboard.tsx
@@ -4,19 +4,64 @@ import { House, MessageCircle, LayoutGrid, Menu } from "lucide-react";
 import NavigationBar from "@/components/navigationBar/navigationBar";
 import { Routes, Route } from "react-router";
 import LandlordHome from "./landlordHome";
+import { useEffect, useState } from "react";
+import { ModeToggle } from "@/components/modeToggle";
+
+type Ttab = "Home" | "Chats" | "Manage" | "Menu";
 
 export default function LandlordDashboard() {
+  const [activeTab, setActiveTab] = useState<Ttab>("Home");
+
+  useEffect(() => {
+    const storedTab = localStorage.getItem("activeTab");
+    if (storedTab) {
+      setActiveTab(storedTab as Ttab);
+    } else {
+      setActiveTab("Home");
+    }
+  }, []);
+
+  function handleTab(param: Ttab) {
+    localStorage.setItem("activeTab", param);
+    setActiveTab(param);
+  }
+
   return (
     <main className="bg-white h-screen relative">
+      <ModeToggle />
       <ResponsiveDialog />
       <Routes>
-        <Route path="" element={<LandlordHome />}/>
+        <Route path="" element={<LandlordHome />} />
       </Routes>
       <NavigationBar>
-        <NavigationItem url="/landlord/dashboard" Icon={House} label="home" />
-        <NavigationItem url="/landlord/dashboard/chats" Icon={MessageCircle} label="Chats" />
-        <NavigationItem url="/landlord/dashboard/manage" Icon={LayoutGrid} label="Manage" />
-        <NavigationItem url="/landlord/dashboard/menu" Icon={Menu} label="Menu" />
+        <NavigationItem
+          url="/landlord/dashboard"
+          Icon={House}
+          label="Home"
+          activeTab={activeTab}
+          onClick={handleTab}
+        />
+        <NavigationItem
+          url="/landlord/dashboard/chats"
+          Icon={MessageCircle}
+          label="Chats"
+          activeTab={activeTab}
+          onClick={handleTab}
+        />
+        <NavigationItem
+          url="/landlord/dashboard/manage"
+          Icon={LayoutGrid}
+          label="Manage"
+          activeTab={activeTab}
+          onClick={handleTab}
+        />
+        <NavigationItem
+          url="/landlord/dashboard/menu"
+          Icon={Menu}
+          label="Menu"
+          activeTab={activeTab}
+          onClick={handleTab}
+        />
       </NavigationBar>
     </main>
   );
